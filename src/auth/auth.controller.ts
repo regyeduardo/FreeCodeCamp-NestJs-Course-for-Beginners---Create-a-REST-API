d7981db08f20ca05312270ba56c9a93fc92ea678
Regy Eduardo <dev@regyeduardo.com>
2023-12-22 22:06:25 -0300

    30:44 Setting up prisma

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+﻿import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller({
+    path: 'auth',
+})
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @Post('signup')
+    signup() {
+        return this.authService.signup();
+    }
+
+    @Post('signin')
+    signin() {
+        return this.authService.signin();
+    }
+}
