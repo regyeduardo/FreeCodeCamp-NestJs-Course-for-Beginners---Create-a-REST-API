2df3694b6b70fc91349b9361eb6d2f2fed4e7647
Regy Eduardo <dev@regyeduardo.com>
2024-01-21 21:10:52 -0300

    Sign up logic

--- nestjs-api-tutorial/src/auth/auth.service.ts
+++ nestjs-api-tutorial/src/auth/auth.service.ts
@@ -1,21 +1,37 @@
-﻿import { Injectable } from '@nestjs/common';
+﻿import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable({})
 export class AuthService {
     constructor(private prisma: PrismaService) {}
 
-    signup(dto: AuthDto) {
-        // generate the password hash
+    async signup(dto: AuthDto) {
+        try {
+            // generate the password hash
+            const hash = await argon.hash(dto.password);
 
-        // save the new user in db
+            // save the new user in db
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                },
+            });
 
-        // return the saved user
-        return {
-            msg: 'I have signed up',
-        };
+            delete user.hash;
+
+            // return the saved user
+            return user;
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Credential taken');
+                }
+            }
+        }
     }
 
     signin() {
